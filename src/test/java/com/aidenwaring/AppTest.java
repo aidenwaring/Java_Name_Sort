bfb558aefc2ec2dd0454ce866e1d38b9a0262aab
aidenwaring <aiden.waring@icloud.com>
2022-01-03 12:37:32 +1000

    Initial commit

---
+++ src/test/java/com/aidenwaring/AppTest.java
@@ -0,0 +1,11 @@
+package com.aidenwaring;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void main() {
+        Assert.assertTrue(true);
+    }
+}
