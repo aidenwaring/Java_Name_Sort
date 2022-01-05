1d042dfb9d9ebeb650fc90fd2a7f6c380ef1b289
awaring <aiden.waring@corelogic.com.au>
2022-01-05 18:51:49 +1000

    Removed other reader methods. Updated pom to include maven JAR plugin for program execution. Provided args input for text file.

--- src/main/java/com/aidenwaring/App.java
+++ src/main/java/com/aidenwaring/App.java
@@ -1,7 +1,21 @@
 package com.aidenwaring;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);  // Create a Scanner object
+        String fileName;
+
+        System.out.println("Welcome to Name Sorter app.");
+
+        if (args.length == 0) {
+            System.out.println("Provide a string that represents a text file.");
+            fileName = input.nextLine();  // Read user input
+        } else {
+            fileName = args[0];
+        }
+        ReadTextFile.streamFile(fileName);
     }
 }
--- src/main/java/com/aidenwaring/ReadTextFile.java
+++ src/main/java/com/aidenwaring/ReadTextFile.java
@@ -1,46 +1,29 @@
 package com.aidenwaring;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ReadTextFile {
 
-    public static String myFile = "src\\main\\resources\\names.txt";
+    public static void main(String[] args) { }
 
-    public static void main(String[] args) throws Exception
-    {
-//        bufferedReaderMethod();
-//        readFileAsString();
-        streamFile();
-    }
 
-//    Method One: BufferedReader
-    public static void bufferedReaderMethod() throws Exception {
-        File file = new File(myFile);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        while ((st = br.readLine()) != null)
-            System.out.println(st);
-    }
-
-//    Method Two: Read File As String
-    public static void readFileAsString() throws Exception {
-        String data = new String(Files.readAllBytes(Paths.get(myFile)));
-        System.out.println(data);
-    }
-
-//    Method Three: String Stream File
-    public static void streamFile() throws IOException {
-        try (Stream<String> stream = Files.lines(Paths.get(myFile))) {
+    public static void streamFile(String file) throws IOException {
+        String sortedFileName = file.replace(".txt", "" + "-sorted.txt");
+        try (Stream<String> stream = Files.lines(Paths.get(file))) {
             List<String> sortedNames = stream.sorted().collect(Collectors.toList());
             sortedNames.forEach(System.out::println);
 //          Note: As of Java 16, Stream.toList() can be used directly on the Stream to get the list
+
+//          File creator to be its own method for testing?
+            Files.write(Paths.get(sortedFileName + "-sorted.txt"), sortedNames, Charset.defaultCharset());
         }
+        System.out.println("Finished: created " + sortedFileName);
     }
 }
